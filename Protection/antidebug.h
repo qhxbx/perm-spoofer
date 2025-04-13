1e018d24c1b3826bc70c1dd2d22026a294d9c130
qhxbx <sohomz2008@gmail.com>
2025-04-13 13:26:38 +0100

    Update antidebug.h

--- Protection/antidebug.h
+++ Protection/antidebug.h
@@ -18,61 +18,27 @@ typedef NTSTATUS(__stdcall* _NtSetInformationThread)(_In_ HANDLE, _In_ THREAD_IN
 
 void AntiDebug(void)
 {
-
+	return true;
 }
 
 int DebugString()
 {
-	LI_FN(SetLastError).safe()(0);
-	LI_FN(OutputDebugStringA).safe()(skCrypt("qhxbx"));
-	const auto last_error = LI_FN(GetLastError).safe()();
-
-	return last_error != 0;
+	return false;
 }
 
 int HideThread()
 {
-	unsigned long thread_hide_from_debugger = 0x11;
-
-	const auto ntdll = LI_FN(LoadLibraryA).safe()(skCrypt("ntdll.dll"));
-
-	if (ntdll == INVALID_HANDLE_VALUE || ntdll == NULL) { return false; }
-
-	_NtQueryInformationProcess NtQueryInformationProcess = NULL;
-	NtQueryInformationProcess = (_NtQueryInformationProcess)LI_FN(GetProcAddress).safe()(ntdll, skCrypt("NtQueryInformationProcess"));
-
-	if (NtQueryInformationProcess == NULL) { return false; }
-
-	(_NtSetInformationThread)(GetCurrentThread(), thread_hide_from_debugger, 0, 0, 0);
-
-	return true;
+	return false;
 }
 
 int RemotePresent()
 {
-	int debugger_present = false;
-
-	LI_FN(CheckRemoteDebuggerPresent).safe()(LI_FN(GetCurrentProcess).safe()(), &debugger_present);
-
-	return debugger_present;
+	return false;
 }
 
 int ContextThread()
 {
-	int found = false;
-	CONTEXT ctx = { 0 };
-	void* h_thread = LI_FN(GetCurrentThread).safe()();
-
-	ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS;
-	if (LI_FN(GetThreadContext).safe()(h_thread, &ctx))
-	{
-		if ((ctx.Dr0 != 0x00) || (ctx.Dr1 != 0x00) || (ctx.Dr2 != 0x00) || (ctx.Dr3 != 0x00) || (ctx.Dr6 != 0x00) || (ctx.Dr7 != 0x00))
-		{
-			found = true;
-		}
-	}
-
-	return found;
+	return false;
 }
 
 #pragma once
@@ -82,70 +48,27 @@ int ContextThread()
 
 inline bool hide_thread(HANDLE thread)
 {
-	typedef NTSTATUS(NTAPI* pNtSetInformationThread)(HANDLE, UINT, PVOID, ULONG);
-	NTSTATUS Status;
-
-	pNtSetInformationThread NtSIT = (pNtSetInformationThread)LI_FN(GetProcAddress).forwarded_safe_cached()((LI_FN(GetModuleHandleA).forwarded_safe_cached())(skCrypt("ntdll.dll")), skCrypt("NtSetInformationThread"));
-
-	if (NtSIT == NULL) return false;
-	if (thread == NULL)
-		Status = NtSIT(LI_FN(GetCurrentThread).forwarded_safe_cached(), 0x11, 0, 0);
-	else
-		Status = NtSIT(thread, 0x11, 0, 0);
-
-	if (Status != 0x00000000)
-		return false;
-	else
-		return true;
+	return false;
 }
 
 inline int thread_context() 
 {
-	int found = false;
-	CONTEXT ctx = { 0 };
-	void* h_thread = LI_FN(GetCurrentThread).forwarded_safe_cached();
-
-	ctx.ContextFlags = CONTEXT_DEBUG_REGISTERS;
-	if (LI_FN(GetThreadContext).forwarded_safe_cached()(h_thread, &ctx))
-	{
-		if ((ctx.Dr0 != 0x00) || (ctx.Dr1 != 0x00) || (ctx.Dr2 != 0x00) || (ctx.Dr3 != 0x00) || (ctx.Dr6 != 0x00) || (ctx.Dr7 != 0x00))
-		{
-			found = true;
-		}
-	}
-
-	return found;
+	return false;
 }
 
 inline int remote_is_present()
 {
-	int debugger_present = false;
-
-	LI_FN(CheckRemoteDebuggerPresent).forwarded_safe_cached()(LI_FN(GetCurrentProcess).forwarded_safe_cached()(), &debugger_present); 
-
-	return debugger_present;
+	return false;
 }
 
 int is_debugger_present()
 {
-	return LI_FN(IsDebuggerPresent).forwarded_safe_cached()(); 
+	return false;
 }
 
 inline bool debug_perms_check() 
 {
-	PCONTEXT ctx = PCONTEXT(LI_FN(VirtualAlloc).forwarded_safe_cached()(NULL, sizeof(ctx), MEM_COMMIT, PAGE_READWRITE));
-	RtlSecureZeroMemory(ctx, sizeof(CONTEXT));
-
-	ctx->ContextFlags = CONTEXT_DEBUG_REGISTERS;
-
-	if (LI_FN(GetThreadContext).forwarded_safe_cached()(LI_FN(GetCurrentThread).forwarded_safe_cached()(), ctx) == 0)
-		return -1;
-
-
-	if (ctx->Dr0 != 0 || ctx->Dr1 != 0 || ctx->Dr2 != 0 || ctx->Dr3 != 0)
-		return TRUE;
-	else
-		return FALSE;
+	return false;
 }
 
 void ProtectionLoop()
