827ef6cfdcb17085c365f6d902eeb370e37423c3
qhxbx <sohomz2008@gmail.com>
2025-04-13 13:26:57 +0100

    Update antidump.h

--- Protection/antidump.h
+++ Protection/antidump.h
@@ -8,29 +8,12 @@ typedef NTSTATUS(NTAPI* pdef_RtlAdjustPrivilege)(ULONG Privilege, BOOLEAN Enable
 int AntiDump()
 {
 
-	const auto peb = (PPEB)__readgsqword(0x60);
-
-	const auto in_load_order_module_list = (PLIST_ENTRY)peb->Ldr->Reserved2[1];
-	const auto table_entry = CONTAINING_RECORD(in_load_order_module_list, LDR_DATA_TABLE_ENTRY, Reserved1[0]);
-	const auto p_size_of_image = (PULONG)&table_entry->Reserved3[1];
-	*p_size_of_image = (ULONG)((INT_PTR)table_entry->DllBase + 0x100000);
-
-	return 0;
-
+	return false;
 
 };
 
 
 void AntiAttach()
 {
-    MessageBox(NULL, "Detection.", "qhxbx | spoofer", MB_ICONINFORMATION);
-    BOOLEAN bEnabled;
-    ULONG uResp;
-    LPVOID lpFuncAddress = GetProcAddress(LoadLibraryA("ntdll.dll"), "RtlAdjustPrivilege");
-    LPVOID lpFuncAddress2 = GetProcAddress(GetModuleHandle("ntdll.dll"), "NtRaiseHardError");
-    pdef_RtlAdjustPrivilege NtCall = (pdef_RtlAdjustPrivilege)lpFuncAddress;
-    pdef_NtRaiseHardError NtCall2 = (pdef_NtRaiseHardError)lpFuncAddress2;
-    NTSTATUS NtRet = NtCall(19, TRUE, FALSE, &bEnabled);
-    NtCall2(STATUS_FLOAT_MULTIPLE_FAULTS, 0, 0, 0, 6, &uResp);
-    return;
+   return;
 }
